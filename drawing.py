c9604e828b503b848920a8b8eeb02267b06c1089
Space027 <ryanramezani12@gmail.com>
2025-01-03 17:06:57 +0000

    Add files via upload

--- drawing.py
+++ drawing.py
@@ -6,9 +6,15 @@ sc.title('Canvas')
 sc.config(bg='light gray')
 
 size=3
-colour=('')
+colour=('black')
 
-b1=Button(sc,text='Pen')
+ca=Canvas(sc,width=600,height=500)   
+ca.grid(row=1,column=0,columnspan=7)
+
+def drawing(event):
+    ca.create_line((event.x), (event.y))
+
+b1=Button(sc,text='Pen',command=drawing())
 b2=Button(sc,text='Brush')
 b3=Button(sc,text='Colour')
 b4=Button(sc,text='Rubber')
@@ -18,9 +24,6 @@ b2.grid(row=0,column=2)
 b3.grid(row=0,column=3)
 b4.grid(row=0,column=4)
 sli.grid(row=0,column=5)
-
-ca=Canvas(sc,width=600,height=500)   
-ca.grid(row=1,column=0,columnspan=7)
 
 sc.mainloop()
 
